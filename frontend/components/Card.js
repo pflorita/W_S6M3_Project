e1844689f5582ff88a368baace92f7dacf37eca3
pflorita <ph.florita@gmail.com>
2024-11-30 08:10:02 -0600

    Updated child components

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React from 'react';
 import Figure from "./Figure";
+import styled from 'styled-components';
+
+const StyledCard = styled.div`
+  border: 1px solid black;
+  border-radius: 12px;
+  padding: 2rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  h2 {
+    color: ${pr => pr.color}
+    font-size: 2.5em;
+    margin: 0 0 1rem 0;
+  }
+
+  p {
+    margin: 0 0 1.5rem 0;
+
+    &::first-line {
+      font-line: 1.5em;
+    }
+  }
+`
 
 export default function Card({ title, text, imageURL, date }) {
   return (
-    <div className='card'>
-    <h2>{title}</h2>
-    <p>{text}</p>
-    <Figure 
-      imageURL={imageURL}
-      caption={date}
-    />
-  </div>
+    <StyledCard color="blue" className='card'>
+      <h2>{title}</h2>
+      <p>{text}</p>
+      <Figure 
+        imageURL={imageURL}
+        caption={date}
+      />
+    </StyledCard>
   )
 }
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledFigure = styled.figure`
+  max-width: 50%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  figcaption {
+    font-style: italic;
+  }
+`;
 
 export default function Figure({ imageURL, caption }) {
   return (
-    <figure>
+    <StyledFigure>
       <img src={imageURL}/>
       <figcaption>Awesome pic taken on {caption}</figcaption>
-    </figure>
+    </StyledFigure>
   )
 }
